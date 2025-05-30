ab48014362a4dbb1501797e1d4a654f96938a230
Meleray <konovma@gmail.com>
2025-05-30 15:04:59 +0200

    Fix attempt 2

--- napari_organoid_analyzer/_orgacount.py
+++ napari_organoid_analyzer/_orgacount.py
@@ -6,8 +6,8 @@ from napari_organoid_analyzer import settings
 
 #update_version_in_mmdet_init_file('mmdet', '2.2.0', '2.3.0')
 import sys
-import os
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+from pathlib import Path
+sys.path.append(str(Path(__file__).resolve().parent.parent))
 
 import torch
 import mmdet
